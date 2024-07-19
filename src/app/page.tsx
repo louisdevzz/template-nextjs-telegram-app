74459689b93fc504aa0614033a4fc0c52fd31aba
vo huu nhan <vohuunhan1310@gmail.com>
2024-07-19 10:44:18 +0700

    repo

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Home() {
+  return (
+    <div className="flex flex-row justify-center">
+      <h1 className="font-semibold text-black">Hi</h1>
+    </div>
+  );
+}
